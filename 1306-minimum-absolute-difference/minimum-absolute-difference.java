5c7710c166f4621b3bb6db7071bee675b6c92da7
Manojreddy123-bit <reddymanoj4778@gmail.com>
2026-09-06 11:36:49 +0530

    Time: 22 ms (33.71%) | Memory: 63.9 MB (34.46%) - LeetSync

---
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int mindiff=Integer.MAX_VALUE;
+        for(int i=1;i<arr.length;i++){
+            mindiff=Math.min(mindiff,arr[i]-arr[i-1]);
+        }
+        List<List<Integer>> list=new ArrayList<>();
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-arr[i-1]==mindiff){
+                list.add(Arrays.asList(arr[i-1],arr[i]));
+            }
+        }
+        return list;
+    }
+}
